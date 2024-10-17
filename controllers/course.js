5002ef6f3242e81ea851a7106926a2227090e377
upasanagithubrit <upasnachoudhary216@gmail.com>
2024-10-17 16:28:00 +0530

    video 3

---
+++ controllers/course.js
@@ -0,0 +1,16 @@
+const Course=require("../models/course")
+const Tag=require("../models/tag")
+const User=require("../models/user")
+const {uploadImageToCloudinary}=require("../utils/imageUploader")
+
+
+exports.createCourse=async(req,res)=>{
+    try{
+        
+
+
+
+    }catch(error){
+
+    }
+}
---
+++ controllers/tag.js
@@ -0,0 +1,56 @@
+const Tag=require('../models/tag')
+
+
+//create tag
+exports.createTag=async(req,res)=>{
+    try{
+        const {name,description}=req.body;
+        if(!name || !description)
+        {
+            res.status(400).json({
+                success:false,
+                message:"all fields are required",
+            })
+        }
+        // create entry in db
+        const tagDetails=await Tag.create({
+            name:name,
+
+            description:description,
+        })
+        console.log(tagDetails);
+        //return respense
+        return res.status(200).json({
+            success:true,
+            message:"tag created successfully",
+        })
+
+
+    }catch{
+        return res.status(500).json({
+            success:true,
+            message:error.message,
+        })
+
+    }
+}
+
+//get all tags
+exports.showAllTags=async(req,res)=>{
+    try{
+        const allTags=await Tag.find({},{name:true,description:true})
+
+           return res.status(200).json({
+            success:true,
+            message:"all tag returnedsuccessfully",
+            allTags,
+        })
+
+    }catch{
+        return res.status(500).json({
+            success:true,
+            message:error.message,
+        })
+
+    }
+}
